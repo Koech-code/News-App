9d862f2501cab3c947300d6e6732f8f54f491ef4
Koech-code <nixon.koech@moringaschool.com>
2021-08-08 22:32:16 +0300

    created two classes for articles and sources

---
+++ app/models.py
@@ -0,0 +1,27 @@
+class Sources:
+    '''
+    This is my sources class which helps create instances of news sources
+    '''
+
+def __init__(self, id , name, description, url, category, language, country):
+    self.id=id
+    self.name=name
+    self.description=description
+    self.url=url
+    self.category=category
+    self.language=language
+    self.country=country
+
+class Articles:
+    '''
+    This is the articles class that will help me create article objects
+    '''
+
+def __init__(self,source, author, title, description, url, urlToImage, publishedAt):
+    self.source=source
+    self.author=author
+    self.title=title
+    self.description=description
+    self.url=url
+    self.urlToImage=urlToImage
+    self.publishedAt=publishedAt
